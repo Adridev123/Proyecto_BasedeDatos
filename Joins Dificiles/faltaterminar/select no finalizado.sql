06bdd276afbf5945be8c35a3be88492e958e2053
Adridev123 <adriel.fuxet32@gmail.com>
2026-05-28 13:29:46 -0300

    Add files via upload

---
+++ Joins Dificiles/faltaterminar/select no finalizado.sql
@@ -0,0 +1,2 @@
+use fg;
+SELECT r.Nombre, COUNT(p.ID_Pedido) as sd FROM Restaurantes r JOIN Pedidos p ON r.ID_Restaurante = p.ID_Restaurante JOIN Platos l ON p.ID_Plato = l.ID_Plato GROUP BY r.Nombre, sd HAVING COUNT(l.ID_Plato) > 5 AND SUM(p.ID_Pedido) > 200;
