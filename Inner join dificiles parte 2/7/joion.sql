43d07f87c4964385e88fa559b82dcac926ff3961
Adridev123 <adriel.fuxet32@gmail.com>
2026-06-01 10:24:24 -0300

    Delete Inner join dificiles parte 2 directory

--- Inner join dificiles parte 2/7/joion.sql
+++
@@ -1,7 +0,0 @@
-use fg;
-select a.Nombre, count(r.ID_Reproduccion) as total
-from Artistas a
-join Reproducciones r on r.ID_Artista = a.ID_Artista
-join Albumes l on l.ID_Album = a.ID_Album
-group by a.Nombre, total 
-having count(Albumes.ID_Album) > 3 and sum(Reproducciones.Cantidad) > 1000000
