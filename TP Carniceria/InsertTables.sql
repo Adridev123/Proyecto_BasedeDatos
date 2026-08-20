f99a8ee44454b0122668ad406161e2677115e4ae
Adridev123 <adriel.fuxet32@gmail.com>
2026-08-20 13:11:48 -0300

    Add files via upload

---
+++ TP Carniceria/InsertTables.sql
@@ -0,0 +1,6 @@
+use db;
+
+insert into Pedidos(IDCarnicero, IDCliente, Fecha, Corte, Monto, MetodoPago) values
+(1, 1, 10-7-26, "Cuadrill", 24000, "Mercado Pago"),
+(2, 2, 10-7-26, "Pata muslo", 40800, "Efectivo"),
+(2, 1, 10-7-26, "Carne picada", 19500, "Tarjeta");
